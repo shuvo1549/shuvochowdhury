df2553798da762a34f58d54eedf983ced77a41dc
Shuvo Chowdhury <shuvocofc@gmail.com>
2022-02-10 03:26:28 +0600

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://iili.io/0xHyVj.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+  {
+    id: 2,
+    img: "https://iili.io/0xHSNn.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+  {
+    id: 3,
+    img: "https://iili.io/0xd6a2.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+  {
+    id: 4,
+    img: "https://iili.io/0xHjPR.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+  {
+    id: 5,
+    img: "https://iili.io/0xH8AX.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+  {
+    id: 6,
+    img: "https://iili.io/0xHv9t.md.png",
+    link: "https://www.facebook.com/shuvo1549/",
+  },
+];
